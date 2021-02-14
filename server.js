b6471fe499bf61af10d7b218abb08807d43025b8
Anna-12-Mer <mammeramna@gmail.com>
2021-02-14 08:53:43 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express'); 
+const graphql = require('express-graphql');  
+const app = express(); 
+
+
+// run on the port 4000:
+app.listen(4000,()=>{
+    console.log("running successfully...")
+})
