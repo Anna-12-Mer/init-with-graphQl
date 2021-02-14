e33613fe32f9a9a1133e93e620daea8b238a02b5
rihab mer <mammeramna@gmail.com>
2021-02-14 21:29:51 +0100

    Testing Queries in Graphiql

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,8 +1,14 @@
 const graphql = require('graphql');
+const _ =require('lodash');
 
-const { GraogQLObjectType, GraphQLString, GraphQLSchema } = graphql;
-
-const BookType = new GraogQLObjectType ({
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+// dummy data 
+var books = [
+    {name: 'Name of the Wind', genre:'Fantasy',id:'1'},
+    {name: 'The Final Empire', genre:'Fantasy',id:'2'},
+    {name: 'The long Earth', genre:'Fantasy',id:'3'},
+];
+const BookType = new GraphQLObjectType ({
     name : 'Book',
     fields:()=>({
         id:     {type: GraphQLString},
@@ -11,7 +17,7 @@ const BookType = new GraogQLObjectType ({
     })
 });
 
-const RootQuery = new GraogQLObjectType({
+const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
     fields:{
         book:{
@@ -19,6 +25,8 @@ const RootQuery = new GraogQLObjectType({
             args:{id:{type: GraphQLString}},
             resolve(parent, args){
                 // code to get data from DB / other source 
+                // using lodash ! insted vanila 
+                return _.find(books,{id:args.id});
             }
         }
     }
