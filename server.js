d3a2011fadf84e82148436aa0fc39a1fd2040a7e
rihab mer <mammeramna@gmail.com>
2021-02-14 09:50:58 +0100

    install graphQl

--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express'); 
-const graphql = require('express-graphql');  
-const app = express(); 
-
-
-// run on the port 4000:
-app.listen(4000,()=>{
-    console.log("running successfully...")
-})
