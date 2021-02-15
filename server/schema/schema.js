5a75d95e34e6a71ee6c448819e5c83412c042d01
Anna-12-Mer <mammeramna@gmail.Com>
2021-02-15 08:31:13 +0100

    type relations

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,37 +1,71 @@
 const graphql = require('graphql');
-const _ =require('lodash');
+const _ = require('lodash');
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+const { GraphQLObjectType,
+    GraphQLString,
+    GraphQLSchema,
+    GraphQLID,
+    GraphQLInt } = graphql;
 // dummy data 
 var books = [
-    {name: 'Name of the Wind', genre:'Fantasy',id:'1'},
-    {name: 'The Final Empire', genre:'Fantasy',id:'2'},
-    {name: 'The long Earth', genre:'Fantasy',id:'3'},
+    { name: 'Name of the Wind', genre: 'Fantasy', id: '1', authorId: '2' },
+    { name: 'The Final Empire', genre: 'Fantasy', id: '2', authorId: '1' },
+    { name: 'The long Earth', genre: 'Fantasy', id: '3', authorId: '3' },
 ];
-const BookType = new GraphQLObjectType ({
-    name : 'Book',
-    fields:()=>({
-        id:     {type: GraphQLString},
-        name:   {type: GraphQLString},
-        genre:  {type: GraphQLString}
+
+var authors = [
+    { name: 'Patrick Rothfuss', age: 44, id: '1' },
+    { name: 'Brandon Sanderson', age: 42, id: '2' },
+    { name: 'Terry Pratchett', age: 66, id: '3' }
+];
+
+// ***Book Type***//
+const BookType = new GraphQLObjectType({
+    name: 'Book',
+    fields: () => ({
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        genre: { type: GraphQLString },
+        author: {
+            type: AuthorType,
+            resolve(parent, args) {
+                console.log(parent);
+                return _.find(authors, { id: parent.authorId })
+            }
+        }
     })
 });
-
+// ***Auther Type***//
+const AuthorType = new GraphQLObjectType({
+    name: 'Author',
+    fields: () => ({
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        age: { type: GraphQLInt }
+    })
+});
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
-    fields:{
-        book:{
+    fields: {
+        book: {
             type: BookType,
-            args:{id:{type: GraphQLString}},
-            resolve(parent, args){
+            args: { id: { type: GraphQLID } },
+            resolve(parent, args) {
                 // code to get data from DB / other source 
                 // using lodash ! insted vanila 
-                return _.find(books,{id:args.id});
+                return _.find(books, { id: args.id });
+            }
+        },
+        author: {
+            type: AuthorType,
+            args: { id: { type: GraphQLID } },
+            resolve(parent, args) {
+                return _.find(authors, { id: args.id })
             }
         }
     }
 });
 
 module.exports = new GraphQLSchema({
-    query:RootQuery
+    query: RootQuery
 });
