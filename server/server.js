d39bf1ffc0c8458eddf4f048b628005c073cffbb
Anna-12-Mer <mammeramna@gmail.Com>
2021-02-16 11:11:25 +0100

    add some updates

--- server/server.js
+++ server/server.js
@@ -1,17 +1,26 @@
-const express = require('express'); 
-const { graphqlHTTP } = require('express-graphql');  
-const schema = require('./schema/schema')
-const app = express(); 
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+
+const app = express();
+
+// Connect to mlab / mongodb atls database
+mongoose.connect('mongodb+srv://gql-amna:amna1234@cluster0.m8npc.mongodb.net/graphql?retryWrites=true&w=majority');
+// event listener
+mongoose.connection.once('open', () => {
+  console.log("Connected to database");
+});
 
 
 // bind express with graphql
-app.use('/graphql',graphqlHTTP({
-      schema ,
-      graphiql: true,
-    }),
-  );
+app.use('/graphql', graphqlHTTP({
+  schema,
+  graphiql: true,
+}),
+);
 
 // run on the port 4000:
-app.listen(4000,()=>{
-    console.log("running successfully...")
+app.listen(4000, () => {
+  console.log("running successfully...")
 })
