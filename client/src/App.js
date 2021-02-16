bf5297a71e0c65a432f9a471e9280e90bdaf6913
Anna-12-Mer <mammeramna@gmail.Com>
2021-02-16 21:32:43 +0100

    add client

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import { Component } from "react";
+import React from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+
+      </div>
+    )
+  }
+}
+
+
+
+export default App;
